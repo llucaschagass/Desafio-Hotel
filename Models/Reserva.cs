95918922dccd4ae2e2b8a2bca85a3415278ee91e
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2022-11-09 08:58:46 -0300

    first commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,43 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count > Suite.Capacidade)
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte!");
+            Hospedes = hospedes;
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+                valor-= valor / 10;
+
+            return valor;
+        }
+    }
+}
---
+++ Models/Suite.cs
@@ -0,0 +1,19 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Suite
+    {
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+        
+        public Suite() { }
+
+        
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+    }
+}
